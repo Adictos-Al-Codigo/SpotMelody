2b20d0159d48e6006b3ead1b4995df550a79b3be
Celorio2001 <jahir.celorio2001@gmail.com>
2023-07-16 13:31:03 -0500

    Reproductor añadido en el activity de lista reprodución

--- app/src/main/java/com/jahircelorio/spotmelody/listaReproduccion.java
+++ app/src/main/java/com/jahircelorio/spotmelody/listaReproduccion.java
@@ -1,8 +1,11 @@
 package com.jahircelorio.spotmelody;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -16,16 +19,22 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
 public class listaReproduccion extends AppCompatActivity {
+
+    ImageView imageView1, imageView2;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     NavigationView navigationView;
     ActionBarDrawerToggle actionBarDrawerToggle;
 
+    private MediaPlayer mediaPlayer;
+    private int selectedMusicResId;
+    private ImageView lastClickedButton;
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
-        }else if (item.getItemId() == android.R.id.home){
+        } else if (item.getItemId() == android.R.id.home) {
             onBackPressed();
             return true;
         }
@@ -37,9 +46,6 @@ public class listaReproduccion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listareproduccion);
 
-
-
-
         toolbar = findViewById(R.id.toolbarLR);
         toolbar.setTitle("Lista de Reproducción");
         setSupportActionBar(toolbar);
@@ -48,42 +54,55 @@ public class listaReproduccion extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layoutLR);
         navigationView = findViewById(R.id.nav_viewLR);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(listaReproduccion.this,drawerLayout,R.string.open,R.string.close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(listaReproduccion.this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = (ImageView) view;
+                reproducirMusica(imageView);
+            }
+        };
+
+        imageView1.setOnClickListener(onClickListener);
+        imageView2.setOnClickListener(onClickListener);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getTitle().toString()){
-                    case  "Principal" : {
-                        Intent intent = new Intent(listaReproduccion.this,PrincipalActivity.class);
+                switch (item.getTitle().toString()) {
+                    case "Principal": {
+                        Intent intent = new Intent(listaReproduccion.this, PrincipalActivity.class);
                         startActivity(intent);
                         break;
                     }
-                    case  "Buscar" : {
-                        Intent intent = new Intent(listaReproduccion.this,MainActivity.class);
+                    case "Buscar": {
+                        Intent intent = new Intent(listaReproduccion.this, MainActivity.class);
                         startActivity(intent);
                         break;
                     }
-                    case  "Artistas" : {
-                        Intent intent = new Intent(listaReproduccion.this,artistasRecomendados.class);
+                    case "Artistas": {
+                        Intent intent = new Intent(listaReproduccion.this, artistasRecomendados.class);
                         startActivity(intent);
                         break;
                     }
-                    case  "Lista de Reproducción" : {
-                        Intent intent = new Intent(listaReproduccion.this,listaReproduccion.class);
+                    case "Lista de Reproducción": {
+                        Intent intent = new Intent(listaReproduccion.this, listaReproduccion.class);
                         startActivity(intent);
                         break;
                     }
 
-                    case "Ver Cuenta" :{
-                        Toast.makeText(listaReproduccion.this,"Ver Cuenta",Toast.LENGTH_SHORT).show();
+                    case "Ver Cuenta": {
+                        Toast.makeText(listaReproduccion.this, "Ver Cuenta", Toast.LENGTH_SHORT).show();
                         break;
                     }
-                    case "Cerrar Sesión" : {
-                        Toast.makeText(listaReproduccion.this,"Cerraste Sesión",Toast.LENGTH_SHORT).show();
+                    case "Cerrar Sesión": {
+                        Toast.makeText(listaReproduccion.this, "Cerraste Sesión", Toast.LENGTH_SHORT).show();
                         break;
                     }
                 }
@@ -92,12 +111,45 @@ public class listaReproduccion extends AppCompatActivity {
         });
     }
 
+    private void reproducirMusica(ImageView view) {
+        int CancionId = 0;
+
+        switch (view.getId()) {
+            case R.id.imageView1:
+                CancionId = R.raw.lalamusic;
+                break;
+            case R.id.imageView2:
+                CancionId = R.raw.copavacia;
+                break;
+        }
+
+        if (lastClickedButton != null && lastClickedButton != view && mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            lastClickedButton.setImageResource(R.drawable.playnegro);
+        }
+
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            view.setImageResource(R.drawable.playnegro);
+        } else {
+            if (mediaPlayer != null) {
+                mediaPlayer.release();
+            }
+            mediaPlayer = MediaPlayer.create(this, CancionId);
+            mediaPlayer.start();
+            view.setImageResource(R.drawable.pausenegro);
+        }
+
+        lastClickedButton = view;
+    }
+
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else{
+        } else {
             super.onBackPressed();
         }
     }
 }
+
